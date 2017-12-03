4c11c65964dc919bdaf60c870f107bae9092fdd9
Janice Darikho <janicedarikho@gmail.com>
2017-12-03 07:37:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested').querySelector('.target');
+}
+
+function increaseRankBy(n) {
+  const ranks = document.getElementsByClassName('ranked-list');
+  for (let i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = (i + parseInt(n)).toString()
+  }
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div');
+}
