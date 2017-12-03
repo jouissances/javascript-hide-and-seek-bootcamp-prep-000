9623ea753f94f29117a03245d11793939e7ae223
Janice Darikho <janicedarikho@gmail.com>
2017-12-03 07:27:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.querySelector()[3];
+  return document.querySelector('#nested').getElementsByClassName('target');
 }
