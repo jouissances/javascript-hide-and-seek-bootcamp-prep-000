0c7db72424320dab2fceb8361f7830643a5c5cf8
Janice Darikho <janicedarikho@gmail.com>
2017-12-03 07:24:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.getElementsByClassName('target');
+  return document.querySelector('#nested').getElementsByClassName('target');
 }
